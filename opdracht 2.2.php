089dc2082ec76045301af1fed80d54c9ade16bb9
Shema Oulad Larbi <shema.ouladlarbi@edu-kw1c.nl>
2020-02-19 11:43:59 +0100

    De opdrachten 2.1 en 2.2 , index.css en index.php heb ik toegevoegd

---
+++ opdracht 2.2.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * User: Shema Oulad Larbi
+ * Date: 12-2-2020
+ * Time: 11:06 AM
+ * File: index.php
+ */
+?>
+<body>
+<header>
+    <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+    <link href="index.css" rel="stylesheet" type="text/css">
+</header>
+<aside>
+    <h2>Menu</h2>
+    <ul>
+        <li>Hoofdstuk 2
+            <ul>
+                <li>
+                    <a href="opdracht%202.1.php">Opdracht 2.1</a>
+                </li>
+                <li>
+                    <a href="opdracht%202.2.php">Opdracht 2.2</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</aside>
+<main id="wrapper">
+    <h2>Uitwerkingen</h2>
+</main>
+</body>
