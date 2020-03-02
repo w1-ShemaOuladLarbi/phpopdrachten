71b87988c028c4f38c7157dfcdf5dc4394c50131
Shema Oulad Larbi <shema.ouladlarbi@edu-kw1c.nl>
2020-03-02 13:25:50 +0100

    De opdracht 2.2 heb ik toegevoegd

--- opdracht 2.2.php
+++ opdracht 2.2.php
@@ -1,32 +1,60 @@
-<?php
-/**
+<?php/**
  * User: Shema Oulad Larbi
- * Date: 12-2-2020
- * Time: 11:06 AM
- * File: index.php
- */
-?>
+ * Date: 19-2-2020
+ * Time: 11:38
+ * File: opdracht2.2.php
+ */?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <link rel="stylesheet" href="index.css">
+    <title><?php print "Opdracht 2.2"; ?></title>
+</head>
 <body>
-<header>
-    <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
-    <link href="index.css" rel="stylesheet" type="text/css">
-</header>
-<aside>
-    <h2>Menu</h2>
-    <ul>
-        <li>Hoofdstuk 2
-            <ul>
-                <li>
-                    <a href="opdracht%202.1.php">Opdracht 2.1</a>
-                </li>
-                <li>
-                    <a href="opdracht%202.2.php">Opdracht 2.2</a>
-                </li>
-            </ul>
-        </li>
-    </ul>
-</aside>
-<main id="wrapper">
-    <h2>Uitwerkingen</h2>
-</main>
+<h1>Opdracht 2.2</h1>
+<div>
+    <?php
+    $text1 = "Hallo";
+    $text2 = "Een makkelijke taal";
+    $text3 = "toch zo'n makkelijke taal";
+    $text4 = " wat is ";
+    $text5 = "PHP ";
+    $text6 = "Nooit gedacht dat ";
+    $text7 = "De installatie is best ingewikkeld";
+    $text8 = " Fijn toch";
+    $text9 = "?";
+    $text10 = ".";
+    $text11 = ",";
+    $text12 = "<br>";
+    $text13 = " is";
+    $text14 = " Vind je niet";
+    ?>
+    <h4 class="Opdracht2Punt2H4">
+        Taak 2
+    </h4>
+    <p class="Opdracht2Punt2P">
+        <?php
+        echo $text1.$text11.$text4.$text5.$text3.$text10;
+        echo $text12;
+        echo $text7.$text10.$text8.$text9;
+        echo $text12;
+        echo $text6.$text5.$text3.$text13;
+        ?>
+    </p>
+    <h4 class="Opdracht2Punt2H4">
+        Taak 3
+    </h4>
+    <p class="Opdracht2Punt2P">
+        <?php
+        echo $text1.$text11;
+        echo $text12;
+        echo $text8." dat ".$text5."zo'n makkelijke taal is".$text10;
+        echo $text12;
+        echo $text7.$text10.$text14.$text9;
+        ?>
+    </p>
+</div>
+<a href="index.php">Terug</a>
 </body>
+</html>
