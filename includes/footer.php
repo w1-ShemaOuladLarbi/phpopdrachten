50ee0a6209354eb2cdb948313f8a8a614540903d
Shema Oulad Larbi <shema.ouladlarbi@edu-kw1c.nl>
2020-03-29 22:53:56 +0200

    Opdracht 3.3

--- includes/footer.php
+++ includes/footer.php
@@ -3,3 +3,19 @@ echo "<div class=\"footer\">
     © 2020
 </div>";
 ?>
+<body>
+<div>
+<?php
+$uur = date("H");
+date_default_timezone_set("Europe/Amsterdam");
+if ($uur < "12") {
+    echo "Goedemorgen";
+} elseif ($uur < "17") {
+    echo "Goedemiddag";
+} else {
+    echo "Goedeavond";
+}
+?>
+</div>
+</body>
+<?php
